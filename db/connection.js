dc3d5085a2a90bf3adab3dec856782b34145d067
bradheath96 <bradheath96@gmail.com>
2024-07-19 14:29:43 +0100

    adjusted to host

--- db/connection.js
+++ db/connection.js
@@ -3,17 +3,19 @@ const ENV = process.env.NODE_ENV || 'development';
 
 const config = {};
 
-if (ENV === "production") {
-	config.connectionString = process.env.DATABASE_URL;
-	config.max = 2;
-}
-
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+if (ENV === "production") {
+  console.log(process.env.DATABASE_URL)
+  console.log(process.env.NODE_ENV)
+	config.connectionString = process.env.DATABASE_URL;
+	config.max = 2;
+}
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-	throw new Error("PGDATABASE not set or DATABASE_URL not set");
+	throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
 module.exports = new Pool(config);
